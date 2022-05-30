0eca1ef008e1f9224d0f14bdf464a9ee42d1b9c5
Artem Tochenyy <artem.tochenyy@gmail.com>
2022-05-30 10:44:01 +0300

    fixed mistakes task 17

--- src/by/incubator/task17/codeAnalysis/CodeAnalyzer.java
+++ src/by/incubator/task17/codeAnalysis/CodeAnalyzer.java
@@ -6,175 +6,62 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 
-import static by.incubator.task17.codeAnalysis.Printer.*;
-
 public class CodeAnalyzer {
-
-    public static final String CLASS_NAME = "Class name:";
-    public static final String CLASS_LOADER = "Class loader:";
-    public static final String IMPLEMENTED_INTERFACES = "Implemented interfaces of the class:";
-    public static final String METHODS = "Methods:";
-    public static final String METHOD_PARAMETERS = "Parameters:";
-    public static final String METHOD_RETURN_TYPE = "Return type:";
-    public static final String MODIFIERS = "Modifiers:";
-    public static final String HIERARCHY = "Hierarchy of superclasses:";
-    public static final String FIELDS = "Fields:";
-    public static final String ANNOTATIONS = "Annotations:";
-    public static final String CONSTRUCTORS = "Constructors:";
-    public static final String STATIC = "static";
-    public static final String ABSTRACT = "abstract";
-    public static final String FINAL = "final";
-    public static final String PUBLIC = "public";
-    public static final String PROTECTED = "protected";
-    public static final String PRIVATE = "private";
-
     public static void analyzeCode(Object o) {
         Class<?> clazz = o.getClass();
+        String className = clazz.getName();
+        Printer.printClassName(className);
 
-        print(CLASS_NAME, clazz.getName(), true);
+        String classLoaderName = clazz.getClassLoader().toString();
+        Printer.printClassLoaderName(classLoaderName);
 
-        printClassLoader(clazz);
+        Class<?>[] interfaces = clazz.getInterfaces();
+        Printer.printInterfaces(interfaces);
 
-        printInterfaces(clazz.getInterfaces());
-        
-        printHierarchy(clazz);
+        Field[] fields = clazz.getDeclaredFields();
+        Printer.printFields(fields);
 
-        printFields(clazz.getDeclaredFields());
+        Constructor<?>[] constructors = clazz.getDeclaredConstructors();
+        Printer.printConstructors(constructors);
 
-        printConstructors(clazz.getConstructors());
+        Method[] methods = clazz.getDeclaredMethods();
+        Printer.printMethods(methods);
 
-        printMethods(clazz.getDeclaredMethods());
-
-        printAnnotations(clazz.getAnnotations());
+        Annotation[] annotations = clazz.getDeclaredAnnotations();
+        Printer.printAnnotations(annotations);
     }
 
-    private static void printClassLoader(Class<?> clazz) {
-        ClassLoader classLoader = clazz.getClassLoader();
+    public static String getModifiers(int modifiers) {
+        StringBuilder modifiersStr = new StringBuilder();
 
-        if (classLoader != null) {
-            print(CLASS_LOADER, classLoader.toString(), true);
-            return;
-        }
-
-        print(CLASS_LOADER, null, true);
-    }
-
-    private static void printConstructors(Constructor<?>[] constructors) {
-        print(CONSTRUCTORS);
-
-        for (Constructor<?> constructor : constructors) {
-            Printer.print(MODIFIERS, 1);
-            printModifiers(constructor.getModifiers());
-
-            Printer.print(METHOD_PARAMETERS, 1);
-            printParameters(true, constructor.getParameterTypes());
-
-            System.out.println();
-        }
-
-        printDashes();
-    }
-
-    private static void printFields(Field... fields) {
-        print(FIELDS);
-
-        for (Field field : fields) {
-            print(field.getName(), 1);
-            printParameters(false, field.getType());
-            printModifiers(field.getModifiers());
-
-            printAnnotations(field.getAnnotations());
-
-            System.out.println();
-        }
-
-        printDashes();
-    }
-
-    private static void printHierarchy(Class<?> clazz) {
-        print(HIERARCHY);
-        while (clazz != null) {
-            print(clazz.getName());
-            clazz = clazz.getSuperclass();
-        }
-
-        printDashes();
-    }
-
-    private static void printInterfaces(Class<?>... interfaces) {
-        print(IMPLEMENTED_INTERFACES);
-
-        for (Class<?> interfaze : interfaces) {
-            print(interfaze.getName());
-
-            printMethods(interfaze.getMethods());
-
-            System.out.println();
-        }
-
-        printDashes();
-    }
-
-    private static void printMethods(Method[] methods) {
-        print(METHODS);
-
-        for (Method method : methods) {
-            Printer.print(method.getName(), 1);
-
-            Printer.print(METHOD_RETURN_TYPE, 1);
-            Printer.print(method.getReturnType().getName(), 2);
-
-            Printer.print(MODIFIERS, 1);
-            printModifiers(method.getModifiers());
-
-            Printer.print(METHOD_PARAMETERS, 1);
-            printParameters(false, method.getParameterTypes());
-
-            System.out.println();
-        }
-    }
-
-    private static void printAnnotations(Annotation[] annotations) {
-        print(ANNOTATIONS,1);
-
-        for (Annotation annotation : annotations) {
-            print(annotation.toString(), 2);
-        }
-    }
-
-    private static void printParameters(boolean withAnnotations, Class<?>... parameters) {
-        for (Class<?> parameter : parameters) {
-            Printer.print(parameter.getName(), 2);
-
-            if (withAnnotations) {
-                printAnnotations(parameter.getDeclaredAnnotations());
-            }
-        }
-    }
-
-    private static void printModifiers(int modifiers) {
         if (Modifier.isAbstract(modifiers)) {
-            Printer.print(ABSTRACT, 2);
+            modifiersStr.append("abstract ");
+        }
+        if (Modifier.isFinal(modifiers)){
+            modifiersStr.append( "final ");
+        }
+        if (Modifier.isNative(modifiers)){
+            modifiersStr.append("native ");
         }
 
-        if (Modifier.isFinal(modifiers)) {
-            Printer.print(FINAL, 2);
+        if (Modifier.isPrivate(modifiers)){
+            modifiersStr.append("private ");
+        } else if (Modifier.isProtected(modifiers)){
+            modifiersStr.append("protected ");
+        } else if (Modifier.isPublic(modifiers)){
+            modifiersStr.append("public ");
         }
 
-        if (Modifier.isPublic(modifiers)) {
-            Printer.print(PUBLIC, 2);
+        return modifiersStr.toString();
+    }
+
+    public static String getParameters(Class<?>[] parameterTypes) {
+        StringBuilder paramInfo = new StringBuilder();
+
+        for (Class<?> parameter : parameterTypes) {
+            paramInfo.append(parameter.getTypeName()).append(" ");
         }
 
-        if (Modifier.isProtected(modifiers)) {
-            Printer.print(PROTECTED, 2);
-        }
-
-        if (Modifier.isPrivate(modifiers)) {
-            Printer.print(PRIVATE, 2);
-        }
-
-        if (Modifier.isStatic(modifiers)) {
-            Printer.print(STATIC, 2);
-        }
+        return paramInfo.toString();
     }
 }
--- src/by/incubator/task17/codeAnalysis/Printer.java
+++ src/by/incubator/task17/codeAnalysis/Printer.java
@@ -1,36 +1,110 @@
 package by.incubator.task17.codeAnalysis;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 public class Printer {
-
-    public static final String[] TABS = {"", "\t", "\t\t", "\t\t\t"};
-
-    public static void print(String message) {
-        System.out.println(message);
-    }
-
-    public static void print(String message, int numOfTabs) {
-        checkNumOfTabs(numOfTabs);
-        System.out.println(TABS[numOfTabs] + message);
-    }
-
-    public static void print(String message, String info, boolean withDashes) {
-        System.out.println(message);
-        System.out.println(info);
-
-        if (withDashes) {
-            printDashes();
-        }
-    }
+    public static final String IMPLEMENTED_INTERFACES = "Implemented interfaces of the class:";
+    public static final String METHODS = "Methods:";
+    public static final String METHOD_PARAMETERS = "Parameters:";
+    public static final String METHOD_RETURN_TYPE = "Return type:";
+    public static final String MODIFIERS = "Modifiers:";
+    public static final String FIELDS = "Fields:";
+    public static final String ANNOTATIONS = "Annotations:";
+    public static final String CONSTRUCTORS = "Constructors:";
 
     public static void printDashes() {
         System.out.println("------------------------------------------------------");
     }
 
-    private static void checkNumOfTabs(int numOfTabs) {
-        if (numOfTabs < 0 || numOfTabs > 3) {
-            throw new IllegalArgumentException("number of tabs: " + numOfTabs + " (but must be [0..3])");
+    public static void printClassName(String className) {
+        System.out.println("Classname: " + className);
+        printDashes();
+    }
+
+    public static void printClassLoaderName(String classLoaderName) {
+        System.out.println("Classloader: " + classLoaderName);
+        printDashes();
+    }
+
+    public static void printInterfaces(Class<?>[] interfaces) {
+        System.out.println(IMPLEMENTED_INTERFACES);
+
+        for (Class<?> interfaze : interfaces) {
+            System.out.println(interfaze.getName());
+            printMethods(interfaze.getMethods());
+            System.out.println();
+        }
+
+        printDashes();
+    }
+
+    public static void printMethods(Method[] methods) {
+        System.out.println(METHODS);
+
+        for (Method method : methods) {
+            System.out.println(method.getName());
+
+            System.out.println(METHOD_RETURN_TYPE);
+            System.out.println(method.getReturnType());
+
+            System.out.println(MODIFIERS);
+            printModifiers(CodeAnalyzer.getModifiers(method.getModifiers()));
+
+            System.out.println(METHOD_PARAMETERS);
+            System.out.println(CodeAnalyzer.getParameters(method.getParameterTypes()));
+
+            System.out.println();
         }
     }
 
+    public static void printFields(Field[] fields) {
+        System.out.println(FIELDS);
 
+        for (Field field : fields) {
+            System.out.println(field.getName());
+            printParameters(field.getType());
+            printModifiers(CodeAnalyzer.getModifiers(field.getModifiers()));
+            printAnnotations(field.getAnnotations());
+            System.out.println();
+        }
+
+        printDashes();
+    }
+
+    private static void printParameters(Class<?>... parameters) {
+        for (Class<?> parameter : parameters) {
+            System.out.println(parameter.getName());
+        }
+    }
+
+    public static void printAnnotations(Annotation[] annotations) {
+        System.out.println(ANNOTATIONS);
+
+        for (Annotation annotation : annotations) {
+            System.out.println(annotation.toString());
+        }
+    }
+
+    public static void printConstructors(Constructor<?>[] constructors) {
+        System.out.println(CONSTRUCTORS);
+
+        for (Constructor<?> constructor : constructors) {
+            System.out.println(MODIFIERS);
+            printModifiers(CodeAnalyzer.getModifiers(constructor.getModifiers()));
+
+            System.out.println(METHOD_PARAMETERS);
+            printParameters(constructor.getParameterTypes());
+
+            System.out.println();
+        }
+
+        printDashes();
+    }
+
+    private static void printModifiers(String modifiers) {
+        System.out.println(modifiers);
+    }
 }
